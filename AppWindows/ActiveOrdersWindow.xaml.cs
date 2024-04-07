80b35d6872af9d26cabde5fc80c745424fe1ed2b
Manaila Robert <robert122003@gmail.com>
2024-04-07 13:06:31 +0300

    Worked on ActiveOrdersWindow 0.1V

--- AppWindows/ActiveOrdersWindow.xaml.cs
+++ AppWindows/ActiveOrdersWindow.xaml.cs
@@ -24,17 +24,68 @@ namespace Pizzeria
         {
             InitializeComponent();
         }
+        int n = 0;
 
+
+        private void DataSelected(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = sender as ComboBox;
+            Status orderStatus = (Status)comboBox.SelectedItem;
+            if(orderStatus == Status.Delivered)
+            {
+                StackPanel cbParent = comboBox.Parent as StackPanel;
+                LB_ActiveOrders.Items.Remove(cbParent);
+                // change the status in DB to delivered
+            }
+        }
+
+        private enum Status
+        {
+            Prepering,
+            Cooking,
+            Done,
+            Delivering,
+            Delivered
+        }
         private void BTN_Click(object sender, RoutedEventArgs e)
         {
-            //TextBlock textBlock = new TextBlock();
-            //textBlock.Text = "Order ID 1\n" +
-            //    "Item 1\n" +
-            //    "Item 2\n" +
-            //    "Item 3\n" +
-            //    "----------------\n" +
-            //    "Total: 324 Lei";
-            //LB_ActiveOrders.Items.Add(textBlock);
+            TextBlock textBlock = new TextBlock();
+            StackPanel NewStackPanel = new StackPanel();
+            ComboBox comboBox = new ComboBox();
+
+
+            //ComboBox Proprieties
+            comboBox.ItemsSource = Enum.GetValues(typeof(Status));
+            comboBox.SelectedItem = Status.Prepering;
+            comboBox.Width = 150;
+            comboBox.Height = 20;
+            comboBox.SelectionChanged += DataSelected;
+            comboBox.FontSize = 12;
+
+            //StackPanel Proprieties
+            NewStackPanel.HorizontalAlignment = HorizontalAlignment.Center;
+            NewStackPanel.Orientation = Orientation.Horizontal;
+           
+            //TexttBlock Proprieties
+            n++;
+            textBlock.Text = $"Order ID {n}\n" +
+                "----------------\n" +
+                "Item 1\n" +
+                "Item 2\n" +
+                "Item 3\n" +
+                "----------------\n" +
+                "Total: 324 Lei";
+            textBlock.FontSize = 14;
+
+            /*Adding the children
+             * hierarchy: 
+             * 0: LB_ActiveOrders
+             * 1: NewStackPanel
+             * 2: textBlock + comboBox  
+            */
+            NewStackPanel.Children.Add(textBlock);
+            NewStackPanel.Children.Add(comboBox);
+            LB_ActiveOrders.Items.Add(NewStackPanel);
         }
     }
 }
